f654a74c7b340afa8f4e4bc82a8a01e893d98652
Manuel <manuel_ordonez@bantrab.net.gt>
2025-10-26 03:46:08 -0600

    se cambia main

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-// src/main.ts
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import * as bodyParser from 'body-parser'
@@ -10,17 +9,15 @@ async function bootstrap() {
   app.use(bodyParser.json({ limit: '5mb' }))
   app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }))
 
-  // CORS:
-  // - Permitimos el frontend público (FRONTEND_URL)
-  // - Permitimos localhost:3002 para pruebas locales
-  // - Permitimos '*' como red de seguridad por ahora
-  const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3002'
+  const PROD_FRONT = 'https://visitasfront-production.up.railway.app'
+  const LOCAL_FRONT_1 = 'http://localhost:3002'
+  const LOCAL_FRONT_2 = 'http://127.0.0.1:3002'
 
   app.enableCors({
-    origin: [frontendUrl, 'http://localhost:3002', 'http://127.0.0.1:3002', '*'],
-    credentials: true,
+    origin: [PROD_FRONT, LOCAL_FRONT_1, LOCAL_FRONT_2],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: false,
   })
 
   const port = Number(process.env.PORT ?? 3001)
